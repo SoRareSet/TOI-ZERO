63e58a08fac62780d5f8339b1b5b22784f46550b
Sarnseth_Nett <sanseth.net@gmail.com>
2026-04-27 00:56:51 +0700

    A3 solution

--- A3-010V2.py
+++ A3-010V2.py
@@ -1,4 +1,3 @@
-N , K , T = map(int,input().split())
 def xgcd(i,j):
   x0 , x , y0 , y = [1,0,0,1]
   while j:
@@ -6,6 +5,7 @@ def xgcd(i,j):
       y0 , y = y , y0-i//j*y
       i , j = j , i%j
   return i , x0 , y0
+N , K , T = map(int,input().split())
 g , c , d = xgcd(N,K)
 r , T = N//g , T-1
 if T%g or T==0: print(r)
