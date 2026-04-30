44e5fc351fe873ed730e82f9f959ffc010eeba44
Sarnseth_Nett <sanseth.net@gmail.com>
2026-04-30 11:10:48 +0700

    A2-solution

--- A2-047.py
+++ A2-047.py
@@ -1,15 +1,10 @@
 import re
-tmp = [input().lower() for i in range(int(input()))]
-for i in tmp:
-    if i.find("hello")+1 and not i[i.find("hello")+5].isalnum() :
-        print("Hello! How can I help you?")
-    elif i.find("hi")+1 and not i[i.find("hi")+2].isalnum() :
-        print("Hello! How can I help you?")
-    elif i.find("bye")+1 and not i[i.find("bye")+3].isalnum() :
-        print("Goodbye! Have a nice day!")
-    elif i.find("goodbye")+1 and not i[i.find("goodbye")+7].isalnum() :
-        print("Goodbye! Have a nice day!") 
-    elif i[-1]=='?' : print("That's an interesting question!")
-    elif len(re.findall("[0-9]",i)) : print("I see some numbers there!")
-    elif len(i)-i.count(' ')>19 : print("That's quite a long message!")
-    else : print("I understand.")
+N = int(input())
+for i in [input().strip().lower() for i in range(N)]:
+    j = i.replace("ello","i").replace("good","").split()
+    if "hi" in j: print("Hello! How can I help you?")
+    elif "bye" in j: print("Goodbye! Have a nice day!") 
+    elif i[-1] == "?": print("That's an interesting question!")
+    elif re.findall("[0-9]",i): print("I see some numbers there!")
+    elif len(i)-i.count(" ") > 19: print("That's quite a long message!")
+    else: print("I understand.")
