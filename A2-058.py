bbc59840da8cd27629b09a9b2bb342ce518272f3
Sarnseth_Nett <sanseth.net@gmail.com>
2026-05-01 20:21:15 +0700

    A2-solution
    Refactor logic to adjust values of A and B based on remainder and r.

--- A2-058.py
+++ A2-058.py
@@ -1,6 +1,6 @@
 A , B , d , r = map(int,input().split())
-if A%d>r : A=A//d*d+d
-elif A%d<r : A=A//d*d
-if B%d>r : B=B//d*d
-elif B%d<r : B=B//d*d-d
+if A%d > r: A = (A//d)*d+d+r
+elif A%d < r: A = (A//d)*d+r
+if B%d > r: B = (B//d)*d+r
+elif B%d < r: B = (B//d)*d-d+r
 print(1-(A-B)//d)
