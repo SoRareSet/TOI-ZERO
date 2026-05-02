3a8578bfab694b9b0a4f979236adb79627846f00
Sarnseth_Nett <sanseth.net@gmail.com>
2026-05-02 11:21:34 +0700

    50 point

--- A3-023.py
+++ A3-023.py
@@ -1,9 +1,10 @@
 A , B = map(int,input().split())
-num , C = set([2]+list(range(3,15001,2))) , 0
+num , C = [0]*15002 , 0
 for i in range(3,123,2):
-    if i not in num: continue
-    for j in range(i*i,15001,2*i): num.discard(j)
-for i in num:
+    if num[i]: continue
+    for j in range(i*i,15001,2*i): num[j] = 1
+for i in range(3*A+(A&1==0),3*B,2):
+    if num[i]: continue
     for j in range(A,B+1):
         p = (i-j)//2
         C += max(0,1+min(p-j,B-(i-j)%2-p))
