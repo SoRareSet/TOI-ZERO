7c19f04847c02f12be6193cdced711f456d3d49a
Sarnseth_Nett <sanseth.net@gmail.com>
2026-05-02 14:00:49 +0700

    A2-solution
    Refactor calculation logic for variable C and adjust loop range.

--- A3-023.py
+++ A3-023.py
@@ -1,11 +1,10 @@
 A , B = map(int,input().split())
 num , C = [0]*15002 , 0
-for i in range(3,123,2):
+for i in range(3,115,2):
     if num[i]: continue
     for j in range(i*i,15001,2*i): num[j] = 1
 for i in range(3*A+(A&1==0),3*B,2):
     if num[i]: continue
     for j in range(A,B+1):
-        p = (i-j)//2
-        C += max(0,1+min(p-j,B-(i-j)%2-p))
+        C += max(0,1+min((i-3*j)//2,B-(i-j)//2-((i-j)%2)))
 print(C)
