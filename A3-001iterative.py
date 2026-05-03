04e2f78e6d57821472764f92cd27a2503084791f
Sarnseth_Nett <sanseth.net@gmail.com>
2026-05-03 18:23:03 +0700

    A3-001_alt

---
+++ A3-001iterative.py
@@ -0,0 +1,16 @@
+N , pt = int(input()) , [0]
+pt += [map(int,input().split()) for i in range(N)]
+stack , dp = [(1,False)] , dict(())
+while stack:
+    k , vis = stack.pop()
+    a , l , b , r = pt[k]
+    if vis:
+        wl = l if a else dp[l]
+        wr = r if b else dp[r]
+        pt[0] += abs(wl-wr)
+        dp[k] = 2*max(wl,wr)
+    else:
+        stack.append((k,True))
+        if not a: stack.append((l,False))
+        if not b: stack.append((r,False))
+print(pt[0])
