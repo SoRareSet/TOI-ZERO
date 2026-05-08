b2f9c930b01aba482f14a97ad5d4cc44db7dd184
Sarnseth_Nett <sanseth.net@gmail.com>
2026-05-08 12:45:31 +0700

    Updated-solution
    Refactor code to simplify counting logic and improve readability.

--- A2-008.py
+++ A2-008.py
@@ -1,6 +1,6 @@
 tmp = sorted([list(map(int,input().split())) for i in range(int(input()))])
-cnt , p , v = 0 , 0 , 0
-for i0 in tmp :
-    if i0[1]>v : p , v = i0
+cnt , v = 0 , 0
+for _ , p in tmp :
+    if p>v : v = p
     else : cnt += 1
 print(cnt)
