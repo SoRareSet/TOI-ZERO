8ad799b35cf98082f7ff468aa4f3d84bf0cf09f8
Sarnseth_Nett <sanseth.net@gmail.com>
2026-05-08 18:56:18 +0700

    A1-solution

---
+++ C++/A1-001.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#define txt substr(0,2)
+using namespace std;
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    string R,S;
+    cin >> R >> S;
+    cout << "Hello "+R+" "+S+"\n";
+    return !bool(cout << R.txt+S.txt);
+}
