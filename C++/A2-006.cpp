7c7a66bdd7280268272c3a9a095f7573d2de17c4
Sarnseth_Nett <sanseth.net@gmail.com>
2026-05-10 11:24:24 +0700

    A2-solution

--- C++/A2-006.cpp
+++ C++/A2-006.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 using namespace std;
-char m[32][32];
-int pt(int i=0,int j=0){
-    if(m[i][j]!='.') return 0;
-    m[i][j]='X';
-    return 1+pt(i,j+1)+pt(i+1,j);
-}
 int main(int ac,char** av){
-    cin.tie(nullptr)->sync_with_stdio(false);
+    cin.tie(0)->sync_with_stdio(0);
     cin>>ac;
-    for(int i=ac,j;i--;)for(j=ac;j--;cin>>m[i][j]);
-    cout<<pt();
+    av = new char*[ac+1]{};
+    for(int i=ac,j;i--;){
+        av[i] = new char[ac+1]{};
+        for(j=ac;j--;cin>>av[i][j]);
+    }
+    av[ac] = new char[ac+1]{};
+    auto pt = [&](auto&& re,int i,int j){
+        if(av[i][j]!='.') return 0;
+        av[i][j]='X';
+        return 1+re(re,i,j+1)+re(re,i+1,j);
+    };
+    return !bool(cout<<pt(pt,0,0));
 }
