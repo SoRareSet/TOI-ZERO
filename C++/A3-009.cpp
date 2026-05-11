b8534c0c68ad85b94e1f013d3f97a8b8fe28cf07
Sarnseth_Nett <sanseth.net@gmail.com>
2026-05-11 14:16:10 +0700

    A3-solution

---
+++ C++/A3-009.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    int N,K;
+    cin>>N>>K;
+    int pt[K]={},mn=N;
+    for(int i=0,j;i++<N;pt[--j]++)
+        cin>>j;
+    for(int p:pt) if(p<mn) mn=p;
+    return !bool(cout<<N-mn*K);
+}
