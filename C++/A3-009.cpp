4a4cd65e7ccda90d5d37812e53d5055883c476a9
Sarnseth_Nett <sanseth.net@gmail.com>
2026-05-11 14:29:43 +0700

    A3-solution

--- C++/A3-009.cpp
+++ C++/A3-009.cpp
@@ -1,9 +1,9 @@
+#pragma GCC optimize("O3")
 #include <iostream>
 using namespace std;
 int main(){
     cin.tie(0)->sync_with_stdio(0);
-    int N,K;
-    cin>>N>>K;
+    int N,K; cin>>N>>K;
     int pt[K]={},mn=N;
     for(int i=0,j;i++<N;pt[--j]++)
         cin>>j;
