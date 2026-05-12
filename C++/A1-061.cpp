5d3f77c3cd97df892bfe8d5f523f4f62a016c65d
Sarnseth_Nett <sanseth.net@gmail.com>
2026-05-12 19:12:57 +0700

    A1-solution

--- C++/A1-061.cpp
+++ C++/A1-061.cpp
@@ -5,7 +5,6 @@ int main(){
     long long X , Y , R;
     cin >> X >> Y >> R;
     X *= X , Y *= Y , R *= R;
-    for(int j=0;j++<ac;cout<<(j%5?'*':'X'));
     if(X + Y > R) cout<<"OUT";
     else if(X + Y < R) cout<<"IN";
     else cout<<"ON";
