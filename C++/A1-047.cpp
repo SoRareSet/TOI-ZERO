3111ab1cfe22b3da25b321a9052a640fcaf6e033
Sarnseth_Nett <sanseth.net@gmail.com>
2026-05-12 16:48:34 +0700

    A1-solution

--- C++/A1-047.cpp
+++ C++/A1-047.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main(){
     cin.tie(0)->sync_with_stdio(0);
     int h,m; cin>>h>>m;
-    m*=h,h=m/60,m%=60;
+    m*=h , h=m/60 , m%=60;
     if(h) cout<<h<<" hours ";
     if(m) cout<<m<<" minutes";
-    if(!(h+m)) cout<<"No teaching";
+    if(h+m==0) cout<<"No teaching";
 }
