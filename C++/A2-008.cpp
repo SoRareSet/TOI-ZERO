23a6780f9c43913b42b3360516d74c8ee744071b
Solver <sanseth.net@gmail.com>
2026-05-13 20:48:53 +0700

    A2-solution

--- C++/A2-008.cpp
+++ C++/A2-008.cpp
@@ -2,11 +2,10 @@
 using namespace std;
 int main(){
     cin.tie(0)->sync_with_stdio(0);
-    int mn=2E9,N,cnt=0;
+    int N,mx=0,cnt=0;
     cin >> N;
-    for(int i=0,p,v;N--;){
-        cin >> p >> v;
-        v>mn?cnt++:mn=v;
-    }
+    vector<int> v(N);
+    for(int p;N--;cin >> p >> v[i]);
+    for(int p:v) p<mx ? cnt++:mx=p;
     cout << cnt;
 }
